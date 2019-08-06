b0b408b07ad650bbbb2ae621d7d517d8a9eedf01
Nidula Chithwara <46299110+nidula@users.noreply.github.com>
2019-08-07 01:50:43 +0530

    Add files via upload

---
+++ web.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+   
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/web.css">
+  <link rel="stylesheet" href="fonts/fontawesome/css/all.css">
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+<div class="container-fluid ni">
+  <div class="button">
+<center>
+<a href="web.php" class="btn"><b>HOME</b></a>
+<div class="dropdown">
+    <button class="dropbtn"><b>SOCIETIES</b></button>
+    <div class="dropdown-content">
+    <a href="loginb.php">BUDDHIST SOCIETY</a>
+    <a href="loginc.php">CATHOLIC & CHRISTIAN SOCIETY</a>
+    <a href="loginm.php">ISLAMIC SOCIETY</a>
+    <a href="loginh.php">HINDU SOCIETY</a>
+    </div>
+</div>
+<a href="contact.html" class="btn"><b>CONTACT</b></a>
+<a href="about.html" class="btn"><b>ABOUT</b></a>
+<br>
+</center>
+
+</div>
+</div>
+ <div class="container-fluid tc" align="center">
+    
+    <div class="col-md-6 tb">
+      <img src="images/sketch1562689897111.png" height="175" width="700">
+    </div>
+    <div class="col-md-6 tb">
+      <img src="images/nsbm.png" height="150" width="400">
+    </div>
+    
+  </div>
+<div class="bodywrapper">
+<div class="buttonwrapper">
+<div class="backwrapper">
+  <div class="da">
+  <marquee><font face="arial" size="5" color="black"><i><b>______One Mother______One Family______Buddhisum______Hinduisum______Christianity______Islam______One Mother______One Family______
+    </b></i></font></marquee>
+  </div>
+</div>
+</div>
+<div class="container-fluid du">
+ 
+  <div id="myCarousel" class="carousel slide" data-ride="carousel" align="center">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+      <li data-target="#myCarousel" data-slide-to="3"></li>
+      <li data-target="#myCarousel" data-slide-to="4"></li>
+      <li data-target="#myCarousel" data-slide-to="5"></li>
+      <li data-target="#myCarousel" data-slide-to="6"></li>
+    
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img class="chi" src="images/11887565.jpg">
+  <div class="text"><font size="5">Temple Of Tooth Relic</font></div>
+      </div>
+
+      <div class="item">
+         <img class="chi" src="images/Esala-Pehrara-by-newsfirst.lk_-1.jpg">
+          <div class="text"><font size="5">Esala Perahara</font></div>
+      </div>
+    
+      <div class="item">
+        <img class="chi" src="images/diwali-decoration-for-office.jpg">
+  <div class="text"><font size="5">Thai Pongal</font></div>
+      </div>
+
+      <div class="item">
+         <img class="chi" src="images/Diwali_Festival.jpg">
+  <div class="text"><font size="5">Deewali Festival</font></div>
+      </div>
+
+      <div class="item">
+       <img class="chi" src="images/grand-mosque-sunset-inside.jpg">
+  <div class="text"><font size="5">Grand Mosque Dubai</font></div>
+      </div>
+
+      <div class="item">
+        <img class="chi" src="images/selling-a-home-at-holidays-ftr.jpg">
+  <div class="text"><font size="5">Christmas</font></div>
+      </div>
+
+      <div class="item">
+        <img class="chi" src="images/church-2464885_960_720.jpg">
+  <div class="text"><font size="5">Church</font></div>
+      </div>
+
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+
+
+<br><br>
+<div class="container">
+  <h1><b>What all religions do...</b></h1>
+  <p><font size="6"><i>The temple, the church, the mosque, the kovil may be different from one another. The idol or the symbol in them may not also be the same and the rites performed in them may be different. But the Paramatman who wants to grace the worshipper, whatever be his faith, is the same. Every religion shows the correct path of the life.
+  </i></font></p>
+</div>
+<br>
+<div class="container">
+  <h1><font color=#ffffff ><b>Great sayings.....</b></font></h1>
+</div>
+<br>
+<div class="container-fluid box">
+<div class="col-md-3">
+<div class="txt"><b>"The religion of the future will be a comic religion. It should trascend a personal god and avoid dogmas and theology. Convering both the natural and the spiritual, it should be based on a religious sense arising from the experience of all things, natural and spiritual, as a meaningful unity. Buddhism answers this description"
+  </b><br><br>
+  -Albert Ainstain-
+</div></div>
+<div class="col-md-3">
+<div class="txt"><b>"This is my simple religion. There is no need for temples; no need for complicated philosophy. Our own brain, our own heart is our temple; the pholosophy is kindness."</b><br><br>-Dalai Lama-
+</div></div>
+<div class="col-md-3">
+<div class="txt"><b>"The history of our spiritual life is a continuing search for the unity between ourselves and the world. Religion, art, and science follow, one and all, this aim."</b><br><br>-Rudolf Stainer-
+</div></div>
+<div class="col-md-3">
+<div class="txt"><b>"I do believe that the original sources of all religions should be taught, because with that we will find our similarities, not just our differences. I believe that if Mohammed, Buddha, Jesus, and Moses all got together they would be best of friends because the spiritual basis of all religions is something that builds unity.""
+</b><br><br>-Yehuda Berg-
+</div></div>
+</div>
+
+<div class="container-fluid ka">
+<div class="bottom" align="center">
+      <a class="btnn" href="https://www.facebook.com/nsbm.lk/">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a class="btnn" href="https://twitter.com/nsbm_srilanka?lang=en">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a class="btnn" href="http://nsbm.lk/">
+        <i class="fab fa-google"></i>
+      </a>
+      <a class="btnn" href="https://www.instagram.com/nsbm_green_university/?hl=en">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a class="btnn" href="https://www.youtube.com/channel/UCHsodhRyiuri2jD7H7nfsRg">
+        <i class="fab fa-youtube"></i>
+      </a>
+    </div>
+  </div>
+</body>
+</html>
